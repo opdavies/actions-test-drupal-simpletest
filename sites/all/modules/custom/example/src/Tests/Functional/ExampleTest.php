b6bfd14ecf24ad3ea40a37c1abd21f39232bff16
Oliver Davies <oliver@oliverdavies.uk>
2020-09-22 09:00:00 +0100

    Add example module

---
+++ sites/all/modules/custom/example/src/Tests/Functional/ExampleTest.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Drupal\example\Tests\Functional;
+
+final class ExampleTest extends \DrupalWebTestCase {
+
+  public function getInfo(): array {
+    return [
+      'name' => 'Example Web Test',
+      'description' => 'An example web test.',
+      'group' => 'Custom',
+    ];
+  }
+
+  public function testThatItReturnsTrue(): void {
+    $this->assertTrue(TRUE);
+  }
+
+}
